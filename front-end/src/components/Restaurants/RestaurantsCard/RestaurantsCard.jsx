7e5d801677e2fa62f317bd8fcfb6917330d83788
Tatiane Entler <94848789+tatientler@users.noreply.github.com>
2022-09-05 15:11:49 -0300

    Adding React Components

--- front-end/src/components/Restaurants/RestaurantsCard/RestaurantsCard.jsx
+++ front-end/src/components/Restaurants/RestaurantsCard/RestaurantsCard.jsx
@@ -7,7 +7,6 @@ import {
 
 import { LikeButton } from "../LikeButton/LikeButton";
 
-import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
@@ -25,27 +24,27 @@ export function RestaurantsCard(
 ) {
     return (
         <>
-            <Col sm={12} md={6} lg={4} className="restaurant-card mb-3 mt-1">
-                <Card  style={{ width: '100%' }}>
+            <Col sm={12} md={6} lg={4} className="mb-2 mt-1">
+                <Card border="light" className="restaurant-card" style={{ width: '100%' }}>
                     <Card.Body>
-                        <Row className="teste">
-                            <Col span={4} className="align-self-center justify-content-center">
-                            <img className="img-logo" src={restaurantAvatar} alt="Logo do restaurante" />
+                        <Row>
+                            <Col xs={2} md={3} lg={4} className="d-flex align-self-center justify-content-center">
+                                <img className="img-logo" src={restaurantAvatar} alt="Logo do restaurante" />
                             </Col>
-
-                            <Col span={6}>
-                                <Card.Title className="h2 text-overflow">
+                        
+                            <Col xs={8} md={7} lg={6}>
+                                <Card.Title className="h2 text-overflow" onClick={() => getRestaurant(id)}>
                                     {restaurantName} &nbsp;
                                     <BsPatchCheckFill className="check-icon" />
                                 </Card.Title>
 
-                                <Card.Text className="restaurant-description">
+                                <Card.Text className="restaurant-description d-flex align-items-center">
                                     <BsStarHalf className="star-icon" />&nbsp;
                                     <span> {restaurantRating}</span>&nbsp;|&nbsp;
 
                                     <span>R$ 5,60</span>
                                 </Card.Text>
-                                <Card.Text className="restaurant-description">
+                                <Card.Text className="restaurant-description d-flex align-items-center">
                                     <BsStopwatch className="icon" />&nbsp;
                                     <span>30min - 50min |&nbsp;</span>
 
@@ -53,44 +52,14 @@ export function RestaurantsCard(
                                     <span> 2km</span>
                                 </Card.Text>
                             </Col>
-
-                            <Col span={2} className="align-self-center justify-content-end">
-                            <LikeButton />
+                        
+                            <Col xs={2} md={2} lg={2} className="d-flex align-self-center justify-content-center">
+                                <LikeButton />
                             </Col>
                         </Row>
                     </Card.Body>
                 </Card>
             </Col>
-            <div className="restaurant-card mb-3 mt-1 col-sm-12 col-md-6 col-xl-4 d-flex">
-                <div className="align-self-center">
-                    <img className="img-logo me-3 ms-1" src={restaurantAvatar} alt="Logo do restaurante" />
-                </div>
-
-                <div className="col-8" onClick={() => getRestaurant(id)}>
-                    <h2 className="h2 text-overflow" id="1">
-                        {restaurantName} &nbsp;
-                        <BsPatchCheckFill className="check-icon" />
-                    </h2>
-
-                    <p className="restaurant-description d-flex align-items-center">
-                        <BsStarHalf className="star-icon" />&nbsp;
-                        <span> {restaurantRating}</span>&nbsp;|&nbsp;
-
-                        <span>R$ 5,60</span>
-                    </p>
-
-                    <p className="restaurant-description d-flex align-items-center">
-                        <BsStopwatch className="icon" />&nbsp;
-                        <span>30min - 50min |&nbsp;</span>
-
-                        <BsFillGeoFill className="gps-icon" />&nbsp;
-                        <span> 2km</span>
-                    </p>
-                </div>
-
-                <LikeButton />
-
-            </div>
         </>
     )
 }
