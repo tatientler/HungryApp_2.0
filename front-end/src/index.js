6f033570106d251f3ca9a48faeca7f8ead5aaf19
Tatiane Entler <94848789+tatientler@users.noreply.github.com>
2022-09-14 11:05:16 -0300

    Update index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-import './styles/normalize.css'
-import './styles/head.css'
+import reportWebVitals from './reportWebVitals';
+
+import { App } from './App';
+
+import './styles/normalize.css';
+import './styles/head.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
